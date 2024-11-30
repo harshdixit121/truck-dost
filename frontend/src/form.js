1c5c96bd505e7e8dbee66c4bed556a0c50fa5414
harshdixit121 <21cse140@glbitm.ac.in>
2024-11-30 22:10:33 +0530

    Form of Truck Dost

---
+++ frontend/src/form.js
@@ -0,0 +1,27 @@
+function form(){
+    <div className = "contianer">
+        <h1>Truck Dost </h1>
+        <form>
+            <label htmlFor="firstname">First className</label>
+            <input type = "text" placeholder ="Enter First Name" name ="firstname"/> 
+
+            <label htmlFor="lastname">Last className</label>
+            <input type = "text" placeholder ="Enter Last Name" name ="lastname"/> 
+
+            <label htmlFor="password">Password</label>
+            <input type = "password" placeholder ="Enter password" name ="password"/> 
+
+            <label htmlFor="email">Email</label>
+            <input type = "email" placeholder ="Enter Email" name ="email"/>
+
+            <label htmlFor="contact">Contact</label>
+            <input type = "text" placeholder ="Enter your Contact" name ="contact"/>
+
+            <label htmlFor="date">Date</label>
+            <input type = "date" placeholder ="Enter Date" name ="date"/>
+
+            <button type ="">Reset</button>
+            <button type ="submit">Submit</button>
+        </form>
+    </div>
+}
